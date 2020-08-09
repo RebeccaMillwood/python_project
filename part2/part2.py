14559a0e606bac0de4eb00cbe55b17119b19fe5d
RebeccaMillwood <bec-millwood@live.com.au>
2020-08-09 12:39:15 +1000

    added functions for converting date, converting temp, and formatting temp

--- part2/part2.py
+++ part2/part2.py
@@ -1,3 +1,53 @@
 import json
+from datetime import datetime
 import plotly.express as px
 import pandas as pd
+
+DEGREE_SYBMOL = u"\N{DEGREE SIGN}C"
+
+def format_temperature(temp):
+    """Takes a temperature and returns it in string format with the degrees and celcius symbols.
+    
+    Args:
+        temp: A string representing a temperature.
+    Returns:
+        A string contain the temperature and "degrees celcius."
+    """
+    return f"{temp}{DEGREE_SYBMOL}"
+
+def convert_date(date):
+    """Converts and ISO formatted date into a human readable format.
+    
+    Args:
+        iso_string: An ISO date string..
+    Returns:
+        A date formatted like: Weekday Date Month Year
+    """
+    d = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S%z")
+    return d.strftime("%A %d %B %Y")
+
+def convert_f_to_c(temp_in_farenheit):
+    """Converts an temperature from farenheit to celcius
+
+    Args:
+        temp_in_farenheit: integer representing a temperature.
+    Returns:
+        An integer representing a temperature in degrees celcius.
+    """
+    farenheit = float(temp_in_farenheit)
+    celsius = round((farenheit - 32) * (5/9),1)
+    return celsius
+
+
+with open(forecast_file) as json_file:
+    forecast_5days_b = json.load(json_file)
+
+
+
+
+
+# graph 1: min and max temps for each day
+# graph 2: min, min "real feel", and min "real feel shade" temps
+
+# convert date
+# convert temp
